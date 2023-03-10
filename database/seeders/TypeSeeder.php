f59ec52b30a01e8a89e31c2f62b0102a50116457
DarioGiusino <dario.giusino@gmail.com>
2023-03-10 16:10:25 +0100

    defined TypeSeeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,16 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator;
 
 class TypeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Generator $faker): void
     {
-        //
+        // types
+        $types = ['Front-end', 'Back-end', 'Full Stack', 'UI/UX', 'Design'];
+
+        foreach ($types as $type) {
+            $new_type = new Type();
+            $new_type->label = $type;
+            $new_type->color = $faker->hexColor();
+            $new_type->save();
+        }
     }
 }
